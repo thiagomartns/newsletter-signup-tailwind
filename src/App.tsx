89d49aafd37604b1eb6d7dc89f07e247139649a5
thiago <tmg3.mn@gmail.com>
2023-06-15 15:19:51 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
