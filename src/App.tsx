617ab9a97b655a6ec1a0da8d45454dc3ca609112
thiago <tmg3.mn@gmail.com>
2023-06-16 17:48:57 -0300

    Create container for content

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import './App.css'
 function App() {
 
   return (
-    <div className='w-screen h-screen flex items-center justify-center'>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+    <div className='w-screen h-screen flex items-center justify-center bg-mainBackground'>
+      <div className='bg-white w-[628px] h-[441px] rounded-3xl flex items-center justify-center'>
+        <h1 className="text-3xl font-bold underline text-tomato">
+          Hello world!
+        </h1>
+      </div>
     </div>
   )
 }
