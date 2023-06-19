baed802abcd9609868afe047982753c9f8410a48
thiago <tmg3.mn@gmail.com>
2023-06-19 11:02:36 -0300

    Update global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,8 @@ export default {
         background: '#1e1e1e',
         tomato: '#ff6155',
         darkNavy: '#242742',
-        grey: 'rgba(25, 24, 43, 0.25)',
+        greyCustom: 'rgba(25, 24, 43, 0.25)',
         gradient: 'linear-gradient(204.47deg, #FF6A3A 0%, #FF527B 100%)'
-      },
-      typography: {
-        heading: '700 3.5rem/100% "Roboto", sans-serif',
-        body: '400 1rem/150% "Roboto", sans-serif',
-        bodyBold: '700 1rem/150% "Roboto", sans-serif',
-        bodySmall: '700 .75rem/150% "Roboto", sans-serif'
       }
     }
   },
